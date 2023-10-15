2bc5238750d64af0940ddeadcdf04ca82f0d8a9a
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-10-15 22:43:16 +0700

    first commit
    page: Home

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div
+        class="flex navbar justify-between min-h-[60px] items-center px-10 bg-gradient-to-r from-[#09fdee] to-[#16aafe] border-1">
+        <div>
+            <NuxtLink to="/">Bilibili Story Builder</NuxtLink>
+        </div>
+        <nav class="flex items-center gap-[50px]">
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/playgroud">Playgroud</NuxtLink>
+            <NuxtLink to="/contact">Contact</NuxtLink>
+        </nav>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="min-h-screen bg-gradient-to-r from-[#09D4FD] to-[#167BFE]">
+        <Navbar />
+
+        <div class="flex">
+            <div class="max-w-6xl mx-auto text-white">
+                <div class="flex justify-between text-black items-center min-h-[80vh]">
+                    <div class="w-full text-white p-6">
+                        <h1 class="text-7xl leading-normal">
+                            Share loving anime to your <b>Story</b>
+                        </h1>
+                        <div class="flex items-center">
+                            <a href="#getstarted">
+                                <div
+                                    class="bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] rounded-[40px] p-2 px-5 w-fit text-black hover:text-white transition-all duration-200 hover:scale-[102%]">
+                                    <h4>Get Started</h4>
+                                </div>
+                            </a>
+                            <div class="bg-white/40 rounded-[40px] p-2 px-5 w-fit text-black hover:text-white transition-all duration-200 hover:scale-[102%] ml-2">
+                                <h4 class="flex items-center"><img src="img/github-icon.png" class="w-[25px] h-[25px] mr-1 hover:fill-white"> Github</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full">
+                        <div class="bg-white/40 rounded-[60px] p-5">
+                            <img
+                                src="https://user-images.githubusercontent.com/81503963/273407994-24652417-4352-4dd6-9281-284f3d87ad3c.gif">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
