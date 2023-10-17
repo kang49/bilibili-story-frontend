4d05899c0359ca10fa0aef75c68e7bfb52147fcb
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-10-18 01:08:13 +0700

    Home: completed

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
                                 <h4 class="text-white text-[21px] max-w-[300px]">Download the Bilibili Story
                                     Builder Shortcut</h4>
                             </div>
-                            <a href="https://github.com/kang49/bilibili-story-sharing" target="_blank">
+                            <a href="https://github.com/kang49/bilibili-story-sharing/releases/latest/download/Bilibili.Story.Builder.shortcut">
                                 <div
                                     class="ml-[90px] mt-[30px] rounded-[40px] p-[5px] px-5 w-fit text-black bg-gradient-to-r from-[#FFF7AD] to-[#FFA9F9] hover:text-white transition-all duration-200 hover:scale-[102%]">
                                     <h4 class="flex items-center">Download</h4>
@@ -101,7 +101,7 @@
                     </div>
                     <div class="items-end mt-[30px]">
                         <div class="text-white min-h-[50px]">
-                            <a href="#">
+                            <a href="#sub4">
                                 <div
                                     class="mt-[30px] rounded-[40px] p-[5px] px-5 w-fit text-white bg-gradient-to-r from-[#94B9FF] to-[#FFA9F9] hover:text-white transition-all duration-200 hover:scale-[102%]">
                                     <h4 class="flex items-center">Next<i class="fas fa-angle-double-down text-2xl mx-2"></i>
@@ -111,6 +111,77 @@
                         </div>
                     </div>
                 </div>
+                <div id="sub4" class="px-50 min-h-screen">
+                    <div class="flex mt-5">
+                        <div>
+                            <div class="mt-[70px] relative">
+                                <img class="absolute object-cover h-[550px] ml-[100px]" src="/img/iPhone13 Frame.png">
+                                <img class="object-cover h-[539px] ml-[115px] pt-[10px] rounded-[30px]"
+                                    src="/img/Step2 Gif.gif">
+                            </div>
+                        </div>
+                        <div class="mt-[70px]">
+                            <div class="flex items-center space-x-5 ...">
+                                <h4 class="text-white text-[21px] ml-[110px] font-bold pb-8">Step 3:</h4>
+                                <h4 class="text-white text-[21px] ml-[110px] max-w-[300px]">Run Bilibili Story Builder
+                                    Shortcut</h4>
+                            </div>
+                            <div class="bg-white/20 rounded-[60px] p-5 ml-[200px] mt-[20px] max-w-[550px]">
+                                <h4 class="leading-[50px]">• Open the anime or content that you want to share on
+                                    Bilibili.<br>• Look for the share button (usually represented by an arrow or box with an
+                                    arrow) on the screen and tap it.<br>• From the list of sharing options, select
+                                    Bilibili.Story.Builder.
+                                    <br>• If you are prompted with any permission requests, be sure to answer with Always
+                                    Allow to ensure seamless sharing.
+                                    <br>• Finally, choose your target social media platform within the shortcut.
+                                </h4>
+                            </div>
+                            <a href="https://github.com/kang49/bilibili-story-sharing" target="_blank">
+
+                            </a>
+                        </div>
+                    </div>
+                    <div class="items-end mt-[30px]">
+                        <div class="text-white min-h-[50px]">
+                            <a href="#sub5">
+                                <div
+                                    class="mt-[30px] rounded-[40px] p-[5px] px-5 w-fit text-white bg-gradient-to-r from-[#94B9FF] to-[#FFA9F9] hover:text-white transition-all duration-200 hover:scale-[102%]">
+                                    <h4 class="flex items-center">Next<i class="fas fa-angle-double-down text-2xl mx-2"></i>
+                                    </h4>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="sub5" class="px-50 min-h-screen">
+                    <div class="flex mt-5">
+                        <div class="mt-[70px]">
+                            <h1 class="text-white text-[42px] font-[700]">Good Job !</h1>
+                            <div class="flex items-center space-x-5 ...">
+                                <h4 class="text-white text-[21px] max-w-[700px] leading-[40px] pb-8">Now, the Bilibili Story
+                                    Builder shortcut will handle the rest, and your anime will be shared to your story on
+                                    Bilibili without hassle.</h4>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="absolute mt-[400px]">
+                                <h4 class="text-[42px] font-[700] max-w-[500px]">If you have an issue, please report it here
+                                </h4>
+                                <a href="https://github.com/kang49/bilibili-story-sharing/issues" target="_blank">
+                                    <div class="flex justify-end mr-[70px]">
+                                        <div
+                                            class="mt-[30px] rounded-[40px] p-[5px] px-5 w-fit text-black bg-white hover:text-white transition-all duration-200 hover:scale-[102%]">
+                                            <h4 class="flex items-center">Report Issue</h4>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-</div></template>
+    </div>
+
+    <Footer/>
+</template>
